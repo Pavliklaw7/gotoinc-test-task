89d84dba352523c02ec855df68131eb78053e13e
Vladyslav Pavlik <pavliklaw7@gmail.com>
2024-02-23 19:54:39 +0200

    route fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gotoinc-test-task',
+  base: process.env.NODE_ENV === 'production' ? '/gotoinc-test-task/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
